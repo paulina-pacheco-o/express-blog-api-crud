3b6c5604186955bb4bb12cd398be392b71e9031f
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-04 15:29:11 +0200

    index

---
+++ postsController.js
@@ -0,0 +1,31 @@
+const posts = require('./routers/posts.js');
+
+//INDEX
+const index = (req, res) => {
+  res.json(posts)
+}
+
+//SHOW
+const show = (req, res) => {
+
+}
+
+//STORE
+const store = (req, res) => {
+
+}
+
+//UPDATE
+const update = (req, res) => {
+
+}
+
+//MODIFY
+const modify = (req, res) => {
+
+}
+
+//DESTROY
+const destroy = (req, res) => {
+
+}
