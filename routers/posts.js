fcb628b05b2a31b10df5ee9e240ced203b6266bf
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-04 15:54:22 +0200

    postController

--- routers/posts.js
+++ routers/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const postController = require('../')
+const postController = require('../controller/postsController.js')
 
 //Index
 router.get('/', (req, res) => {
