e1383924c399a9f8441b9b259d197d651f2d65a7
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-04 14:42:34 +0200

    first commit

---
+++ routers/posts.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const router = express.Router();
+
+//Index
+router.get('/', (req, res) => {
+  res.send('Lista post')
+});
+
+//Show
+router.get('/:id', (req, res) => {
+  res.send(`Dettaglio del post con id ${req.params.id}`);
+});
+
+//Create
+router.post('/', (req, res) => {
+  res.send('Creazione di un nuovo post');
+});
+
+//Update
+router.put('/:id', (req, res) => {
+  res.send(`Modifica totale del post con id ${req.params.id}`);
+});
+
+//Delete
+router.delete('/:id', (req, res) => {
+  res.send(`Cancellazione del post con id ${req.params.id}`);
+});
+
+module.exports = router;
+
